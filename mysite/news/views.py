cf4a1cb14788e967ab89912066df7eaba428e7f3
Egor <egorcoders@gmail.com>
2022-08-31 18:38:00 +0500

    ADDED template tags

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,21 +5,17 @@ from .models import Category, News
 
 def index(request):
     news = News.objects.order_by('-published')
-    categories = Category.objects.all()
     context = {
         'news': news,
-        'categories': categories,
     }
     return render(request, 'news/index.html', context)
 
 
 def category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, 'news/category.html', context)

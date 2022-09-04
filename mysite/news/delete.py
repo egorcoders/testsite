559fc5dc916d36d166578431d144e701499c5cc9
Egor <egorcoders@gmail.com>
2022-09-04 23:38:08 +0500

    ADDED delete.py

--- mysite/news/delete.py
+++ mysite/news/delete.py
@@ -38,19 +38,66 @@
 
 # d()
 
-def numbers():
-    lst = []
+# def numbers():
+#     lst = []
 
-    def add_number(number):
-        nonlocal lst
-        lst.append(number)
-        print(sum(lst) / len(lst))
+#     def add_number(number):
+#         nonlocal lst
+#         lst.append(number)
+#         print(sum(lst) / len(lst))
 
-    return add_number
+#     return add_number
 
 
-n = numbers()
-print(n)
-n(3)
-n(4)
-n(8)
+# n = numbers()
+# print(n)
+# n(3)
+# n(4)
+# n(8)
+
+
+# def decorator_h1(fun):
+#     def inner(*args, **kwargs):
+#         print('<h1>')
+#         fun(*args, **kwargs)
+#         print('</h1>')
+#     return inner
+
+
+# def decorator_table(fun):
+#     def inner(*args, **kwargs):
+#         print('<table>')
+#         fun(*args, **kwargs)
+#         print('</table>')
+#     return inner
+
+
+# @decorator_table
+# @decorator_h1
+# def say_name(n):
+#     print(n, 'Misha')
+
+# say_name('Good')
+
+# class Balance:
+#     def __init__(self, name, balance):
+#         self.name = name
+#         self.balance = balance
+
+#     def __repr__(self):
+#         return f'Это аккаунт {self.name}'
+
+#     def __str__(self) -> str:
+#         return f'Это аккаунт пользователя {self.name}'
+
+
+# b1 = Balance('Misha', 25)
+
+# print(b1)
+
+# a = 'hello'
+# b = 'hello'
+
+# c = [1, 2, 3]
+# print(a.__hash__())
+# print(c.__missing__(2))
